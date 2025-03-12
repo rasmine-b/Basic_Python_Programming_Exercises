9f612271cd47625807306e4610c900ecebe5008e
rasmine <blasrasmine@gmail.com>
2025-03-12 19:52:30 +0800

    start and end the loop

--- batch_4_python_programs/displays_the_most_number_of_duplicate.py
+++ batch_4_python_programs/displays_the_most_number_of_duplicate.py
@@ -1,5 +1,12 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 #start a loop that ask user to input a number until it's invalid
-#end the loop if the input is invalid
+num_list = []
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        break #end the loop if the input is invalid
+
 #display the number with the most number of duplicate
