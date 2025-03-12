cbf6a918f461e158eecf2840261d75028731601a
rasmine <blasrasmine@gmail.com>
2025-03-12 19:45:14 +0800

    Updated commit message

--- batch_4_python_programs/numbers_that_have_duplicate.py
+++ batch_4_python_programs/numbers_that_have_duplicate.py
@@ -6,10 +6,10 @@ for i in range (1,11):
     num = float(input(f"Enter a number {i}: "))
     num_list.append(num)
 
-#Find numbers that have duplicate
+#Find numbers that have duplicates
 duplicates = list(set(num for num in num_list if num_list.count(num) > 1))
 
-#Display numbers that have duplicate
+#Display numbers that have duplicates
 if duplicates:
     print(f"The numbers that have duplicates are {duplicates}")
 else:
