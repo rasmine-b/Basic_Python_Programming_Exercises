af2260f080c58fb2ee565f378efc674cb446586a
rasmine <blasrasmine@gmail.com>
2025-03-12 16:58:00 +0800

    pseudocode

---
+++ batch_3-python_programs/display_unique_duplicate_numbers.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+#take 10 numbers as input
+#displayfirst occurence numbers
+#display numbers with duplicate, only once
