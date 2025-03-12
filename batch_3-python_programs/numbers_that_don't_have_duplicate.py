46881b09bf393ddfd2fd47bd6cba3a01201201a7
rasmine <blasrasmine@gmail.com>
2025-03-12 16:44:13 +0800

    input 10 numbers

--- batch_3-python_programs/numbers_that_don't_have_duplicate.py
+++ batch_3-python_programs/numbers_that_don't_have_duplicate.py
@@ -1,5 +1,11 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 #Ask user to input 10 numbers
+duplicate_num_list = []
+for i in range (1,11):
+    num = float(input("Enter a number {i}: "))
+    duplicate_num_list.append(num)
+    
 #Find numbers that don't have duplicate
+
 #Display numbers that don't have duplicate
