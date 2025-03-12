2f238f83677147ab3064eaac528bfcc6196bc101
rasmine <blasrasmine@gmail.com>
2025-03-12 20:04:18 +0800

    pseudocode

---
+++ batch_4_python_programs/display_numbers_from_highest_to_lowest.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+#start a loop that ask user to a valid number until it's invalid
+#end the loop if the input is invalid
+#display numbers from highest to lowest
