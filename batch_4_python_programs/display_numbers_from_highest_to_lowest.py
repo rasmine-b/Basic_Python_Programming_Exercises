0a677e1a10922ef7c5016fb35172b7e525ab6965
rasmine <blasrasmine@gmail.com>
2025-03-12 20:05:19 +0800

    start and end the loop

--- batch_4_python_programs/display_numbers_from_highest_to_lowest.py
+++ batch_4_python_programs/display_numbers_from_highest_to_lowest.py
@@ -1,5 +1,12 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
 #start a loop that ask user to a valid number until it's invalid
-#end the loop if the input is invalid
+num_list = []
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        break #end the loop if the input is invalid
+    
 #display numbers from highest to lowest
