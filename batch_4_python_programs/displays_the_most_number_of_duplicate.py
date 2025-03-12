b718bcf9a36031b3ab50b8a2de81e5f9664410f1
rasmine <blasrasmine@gmail.com>
2025-03-12 19:49:38 +0800

    pseudocode

---
+++ batch_4_python_programs/displays_the_most_number_of_duplicate.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+#start a loop that ask user to input a number until it's invalid
+#end the loop if the input is invalid
+#display the number with the most number of duplicate
