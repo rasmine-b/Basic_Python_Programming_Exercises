7b0c91a20ac145aafa0ed6afc3ee7b713ca4a0f4
rasmine <blasrasmine@gmail.com>
2025-03-12 19:58:33 +0800

    pseudocode

---
+++ batch_4_python_programs/displays_the_highest_number.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number.
+
+#start a loop that ask user a valid number until it's invalid
+#end the loop if the input is invalid
+#display the highest number
