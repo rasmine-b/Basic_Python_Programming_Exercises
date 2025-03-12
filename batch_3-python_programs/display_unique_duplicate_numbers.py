ac414c7926b17d15baaba9c1387503f8e2649fa2
rasmine <blasrasmine@gmail.com>
2025-03-12 17:06:13 +0800

    display numbers

--- batch_3-python_programs/display_unique_duplicate_numbers.py
+++ batch_3-python_programs/display_unique_duplicate_numbers.py
@@ -6,5 +6,11 @@ for i in range (1,11):
     num = float(input(f"Enter a number {i}: "))
     num_list.append(num)
 
-#displayfirst occurence numbers
+#display first occurence numbers
+duplicate_numbers = set()
+unique_numbers = []
+for num in num_list:
+    if num not in duplicate_numbers:
+        unique_numbers.append(num)
+        duplicate_numbers.add(num)
 #display numbers with duplicate, only once
