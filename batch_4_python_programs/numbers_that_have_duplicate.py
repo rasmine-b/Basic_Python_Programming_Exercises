11d5ee26eef679e83043e198e0abde9a5e004f79
rasmine <blasrasmine@gmail.com>
2025-03-12 19:34:00 +0800

    pseudocode

---
+++ batch_4_python_programs/numbers_that_have_duplicate.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+#Ask user to input 10 numbers
+#Find numbers that have duplicate
+#Display numbers that have duplicate
