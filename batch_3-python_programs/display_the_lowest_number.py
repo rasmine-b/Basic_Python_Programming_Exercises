16a68420c803a2b41e78fc9d039f79a865219b5a
rasmine <blasrasmine@gmail.com>
2025-03-12 18:45:39 +0800

    start and end loop

--- batch_3-python_programs/display_the_lowest_number.py
+++ batch_3-python_programs/display_the_lowest_number.py
@@ -1,6 +1,13 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 #start a loop that ask user to input number until it's invalid
-#end the loops if the input is invalid
+num_list = []
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        break #end the loops if the input is invalid
+    
 #display the lowest number
 
