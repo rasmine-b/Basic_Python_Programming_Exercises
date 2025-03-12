728ba3abe1c4513c2167b3f61bae77cd50fdc4bd
rasmine <blasrasmine@gmail.com>
2025-03-12 17:01:21 +0800

    take 10 numbers as input

--- batch_3-python_programs/display_unique_duplicate_numbers.py
+++ batch_3-python_programs/display_unique_duplicate_numbers.py
@@ -1,5 +1,10 @@
 #Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 #take 10 numbers as input
+num_list = []
+for i in range (1,11):
+    num = float(input(f"Enter a number {i}: "))
+    num_list.append(num)
+
 #displayfirst occurence numbers
 #display numbers with duplicate, only once
