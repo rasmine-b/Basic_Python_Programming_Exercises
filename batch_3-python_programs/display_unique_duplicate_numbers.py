5c4216ea518c3731e0c4d513cfee4260d5cad9a4
rasmine <blasrasmine@gmail.com>
2025-03-12 17:08:22 +0800

    Updated pseudocode

--- batch_3-python_programs/display_unique_duplicate_numbers.py
+++ batch_3-python_programs/display_unique_duplicate_numbers.py
@@ -6,11 +6,5 @@ for i in range (1,11):
     num = float(input(f"Enter a number {i}: "))
     num_list.append(num)
 
-#display first occurence numbers
-duplicate_numbers = set()
-unique_numbers = []
-for num in num_list:
-    if num not in duplicate_numbers:
-        unique_numbers.append(num)
-        duplicate_numbers.add(num)
-#display numbers with duplicate, only once
+#display first occurence and duplicate numbers
+#display all numbers
