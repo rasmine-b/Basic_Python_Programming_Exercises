ae850e6ab13125b64839aaba6edf4b21c466c9c9
rasmine <blasrasmine@gmail.com>
2025-03-12 20:11:21 +0800

    pseudocode

---
+++ batch_4_python_programs/displays_the_average.py
@@ -0,0 +1,6 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+#start a loop that ask user to input a valid number until it's invalid
+#end the loop if the input is invalid
+#display the average
+
